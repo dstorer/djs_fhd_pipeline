e033ee0ab80b1006b638793f353d4b21111c1cfd
Dara Storer <darajstorer@gmail.com>
2022-10-21 14:43:18 -0700

    Update wrapper1_arrayJob.sh

--- runScripts/runFHD/wrapper1_arrayJob.sh
+++ runScripts/runFHD/wrapper1_arrayJob.sh
@@ -21,4 +21,4 @@ do
 done
 #t=${obs_file_name:54:-4}
 
-sbatch -- export=obs_file_name=${obs_file_name},outdir=${outdir},version_str=${version_str},band=${band},exants=${exants} -o ${outdir}/FHD_${num_prefix}_${band}.out -N 1 -n 1 --mem=64G -J FHD_cal_${band} -p hera /lustre/aoc/projects/hera/dstorer/Projects/updatedHeraOnFHD/runScripts/runFHD/wrapper2_arrayJob.sh 
+sbatch -- export=obs_file_name=${obs_file_name},outdir=${outdir},version_str=${version_str},band=${band},exants=${exants} -o ${outdir}/FHD_${num_prefix}_${band}.out -N 1 -n 1 --mem=64G -J FHD_cal_${band} -p hera /lustre/aoc/projects/hera/dstorer/Setup/djs_fhd_pipeline/runScripts/runFHD/wrapper2_arrayJob.sh 
