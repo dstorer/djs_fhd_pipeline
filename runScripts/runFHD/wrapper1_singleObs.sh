2e644289005d83fc09c4cb08d437bf2a743b2a48
Dara Storer <darajstorer@gmail.com>
2022-10-21 14:43:49 -0700

    Update wrapper1_singleObs.sh

--- runScripts/runFHD/wrapper1_singleObs.sh
+++ runScripts/runFHD/wrapper1_singleObs.sh
@@ -20,4 +20,4 @@ do
   esac
 done
 
-sbatch --export=obs_file_name=${obs_file_name},outdir=${outdir},version_str=${version_str},band=${band},exants=${exants} -o ${outdir}/FHD_${num_prefix}_${band}.out -N 1 -n 1 --mem=64G -J FHD_cal_${band} -p hera /lustre/aoc/projects/hera/dstorer/Projects/updatedHeraOnFHD/runScripts/runFHD/wrapper2_singleObs.sh 
+sbatch --export=obs_file_name=${obs_file_name},outdir=${outdir},version_str=${version_str},band=${band},exants=${exants} -o ${outdir}/FHD_${num_prefix}_${band}.out -N 1 -n 1 --mem=64G -J FHD_cal_${band} -p hera /lustre/aoc/projects/hera/dstorer/Setup/djs_fhd_pipeline/runScripts/runFHD/wrapper2_singleObs.sh 
